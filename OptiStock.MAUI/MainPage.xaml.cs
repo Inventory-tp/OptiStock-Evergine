b81bc5a3bd4dcfa8edd6a93c1bd42b08843688a6
macsimtv <maximelecouturier@gmail.com>
2024-02-23 13:50:32 +0100

    Init

---
+++ OptiStock.MAUI/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+using OptiStock;
+
+namespace OptiStock.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly MyApplication evergineApplication;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.evergineApplication = new MyApplication();
+            this.evergineView.Application = this.evergineApplication;
+        }
+    }
+}
