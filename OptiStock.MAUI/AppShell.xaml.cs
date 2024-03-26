239fed58b17947fc121f169136ce4c302587b498
Julio-star <58565784+Julio-star@users.noreply.github.com>
2024-03-26 16:33:23 +0100

    feature/login : App Starting but black screen for the moment

--- OptiStock.MAUI/AppShell.xaml.cs
+++ OptiStock.MAUI/AppShell.xaml.cs
@@ -2,9 +2,13 @@ namespace OptiStock.MAUI
 {
     public partial class AppShell : Shell
     {
+
         public AppShell()
         {
             InitializeComponent();
+            //Register all routes
+            Routing.RegisterRoute("login", typeof(LoginPage));
+            Routing.RegisterRoute("home", typeof(HomePage));
         }
     }
 }
