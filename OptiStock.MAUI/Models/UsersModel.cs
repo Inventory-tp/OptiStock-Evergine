3eb1a2001ed8f86d030042f6212a4872e4e53fe3
Julio-star <juleshubt24@gmail.com>
2024-04-16 10:41:09 +0200

    SQL_Lite integration : adding nuggets package, updating user model

---
+++ OptiStock.MAUI/Models/UsersModel.cs
@@ -0,0 +1,20 @@
+﻿using Java.Sql;
+using Java.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptiStock.MAUI.Models
+{
+    internal class UsersModel
+    {
+        public UUID ID { get; set; }
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public String password { get; set; }
+        public UsersRights rights { get; set; }
+    }
+}
